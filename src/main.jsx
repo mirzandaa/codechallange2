6b60459b06aaa297741ae210291ec7c9620e2553
mirzandaa <mirzanda,abimanyu@gmail.com>
2023-12-19 21:03:00 +0700

    dev

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './dist/css/main.css'
+import './style/css/bootstrap.min.css';
+import './style/main.css';
+
 
 import { BrowserRouter } from 'react-router-dom'
 
@@ -14,6 +15,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+
 
 
 
